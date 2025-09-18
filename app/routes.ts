162d60f3b7bda3ed29c473bf2de50f88731bb259
Gustavo Galupo <ggalupo@tractian.com>
2025-09-18 17:51:47 -0300

    feat: add checkout page

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("company-info", "routes/company-info.tsx"),
+  route('checkout', 'routes/checkout.tsx'),
 ] satisfies RouteConfig;
